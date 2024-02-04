867ee381f53605e60223e19bc7f520556bbef9c3
Sudhir Malviya <sudhirmalviya1010@gmail.com>
2024-02-04 22:19:07 +0530

    commit by sudhir

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Address
+from .models import *
 
+admin.site.register(Language)
+admin.site.register(Movie)
 @admin.register(Address)
 class AddressAdmin(admin.ModelAdmin):
     pass
+
--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',home,name="home"),
-    path('city_state_search/',city_state_search,name="city_state_search"),
+    path('home/',home,name="home"),
+    path('search/',search,name="search"),
+    path('city_search/',city_search,name="city_search"),
+    path('all_city_data',all_city_data,name="all_city_data"),
+    path('admindash',admindash,name="admindash"),
+    path('create_movie_page',create_movie_page,name="create_movie_page"),
+    path('create_movie',create_movie,name="create_movie"),
+    path('update_movie',update_movie,name="update_movie")
 ]
+if settings.DEBUG:
+        urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
